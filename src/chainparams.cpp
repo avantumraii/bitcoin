974de203a81c72d9eab2cf4a37ebffa6c7da8357
RodMir <41921124+avantumraii@users.noreply.github.com>
2025-02-11 20:39:25 +0300

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2010 Satoshi Nakamoto
-// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Copyright (c) 2025 avantum raii
+// Copyright (c) 2009-2022 The Avantum Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
